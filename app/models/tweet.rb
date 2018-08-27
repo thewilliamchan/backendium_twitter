7f75d1506f4892440ea22973dd6bf35af818bfce
williamwlchan <william.wlchan@outlook.com>
2018-08-27 18:32:22 +0800

    Re-generate models

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,2 @@
 class Tweet < ApplicationRecord
-  validates :user_id, presence: true
-  validates :message, presence: true, length: { maximum: 140 }
-
-  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180827103054) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
