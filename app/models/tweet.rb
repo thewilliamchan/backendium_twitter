81102b19dfad5f378c146854e8bbd8c040260a44
williamwlchan <william.wlchan@outlook.com>
2018-08-27 08:23:25 +0800

    Add model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ApplicationRecord
+  validates :user_id, presence: true
+  validates :message, presence: true, length: { maximum: 140 }
 
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
-class User < ApplicationRecord
+require 'bcrypt'
 
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 64 }
+  validates :email, presence: true, length: { minimum: 5, maximum: 500 }
+  validates :password, presence: true, uniqueness: true, length: { minimum: 8, maximum: 64 }
+
+  has_many :sessions
+  has_many :tweets
+
+  after_validation :encrypt_password
+
+  private
+    def encrypt_password
+      self.password = BCrypt::Password.create(self.password)
+    end
 end
