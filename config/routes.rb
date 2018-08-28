53e7fd1e0609c8c43f24e0a474ccfedff89a287d
williamwlchan <william.wlchan@outlook.com>
2018-08-28 08:25:17 +0800

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,18 @@ Rails.application.routes.draw do
   get '/feeds'                    => 'feeds#index'
 
   # USERS
-
+  post '/users', to: 'users#create'
 
   # SESSIONS
-
+  post '/sessions', to: 'sessions#create'
+  get '/authenticated', to: 'sessions#authenticated'
+  delete '/sessions', to: 'sessions#destroy'
 
   # TWEETS
-
+  post '/tweets', to: 'tweets#create'
+  delete '/tweets/:id', to: 'tweets#destroy'
+  get '/users/:username/tweets', to: 'tweets#index_by_user'
+  get '/tweets/search/keyword', to: 'tweets#search'
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path'    => 'homepage#index'
