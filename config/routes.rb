b2d58fd4e493f59086d0fded7ca0a92c737d2371
williamwlchan <william.wlchan@outlook.com>
2018-09-09 21:59:27 +0800

    Update tweet search path in routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   delete '/tweets/:id', to: 'tweets#destroy'
   get '/tweets', to: 'tweets#index'
   get '/users/:username/tweets', to: 'tweets#index_by_user'
-  get '/tweets/search/keyword', to: 'tweets#search'
+  get '/tweets/search/:keyword', to: 'tweets#search'
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path'    => 'homepage#index'
